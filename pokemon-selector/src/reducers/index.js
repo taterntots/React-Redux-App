1e438815c5a78ea1ad218256a6f6e4e6c2aa4857
taterntots <matthew.a.weidner@gmail.com>
2019-12-12 14:53:07 -0500

    created my store and default reducer with some initial state

---
+++ pokemon-selector/src/reducers/index.js
@@ -0,0 +1,15 @@
+const initialState = {
+    name: bulbasaur,
+    type: grass,
+    isFetching: false,
+    error: ''
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default reducer;
